4909353b4515731fba8ca4f17441a0ec5f6fb947
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2021-12-28 04:27:03 +0900

    Add files via upload

---
+++ query57.sql
@@ -0,0 +1,49 @@
+--
+--	See query57.sql for get_ddl
+--
+set pagesize 40
+set linesize 80
+
+alter session set NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS'
+/
+
+variable vc_obj_type varchar2(19) ;
+variable vc_obj_name varchar2(30) ;
+variable vc_own_name varchar2(30) ;
+
+begin
+ :vc_obj_type := 'TABLE' ;
+ :vc_obj_name := 'IPNMS_PORT_PERF_PMETL_5MIN' ;
+ :vc_own_name := 'LGU_DATACOLLECTOR' ;
+end ;
+/
+show error
+
+spool query57.out 
+
+SELECT sysdate
+FROM dual
+/
+
+SELECT dbid , name , db_unique_name
+, database_role , created , platform_name
+FROM v$database
+/
+SELECT startup_time , instance_number , instance_name
+, instance_role , version , parallel , thread#
+, host_name
+FROM v$instance
+/
+
+print :vc_obj_type :vc_obj_name :vc_own_name
+
+set pagesize 0
+set long 32767
+
+SELECT dbms_metadata.get_ddl(:vc_obj_type, :vc_obj_name, :vc_own_name)
+FROM dual
+/
+
+set pagesize 40
+set linesize 80
+spool off
