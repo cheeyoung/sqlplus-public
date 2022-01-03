49ad6611247271ea7eb9cce60cf5b5187033a50a
Cheeyoung O <90919724+cheeyoung@users.noreply.github.com>
2022-01-04 01:17:31 +0900

    Add files via upload

---
+++ query2a.sql
@@ -0,0 +1,76 @@
+set pagesize 40
+set linesize 80
+
+--connect / as sysdba
+
+alter session set NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS'
+/
+
+variable vc_sql_id varchar2(13) ;
+variable vc_sql_text varchar2(1000) ;
+
+begin
+ :vc_sql_id := '520mkxqpf15q8' ;
+end ;
+/
+show error
+
+column dbid format 9999999999999
+
+spool query2a.out
+
+print :vc_sql_id :vc_sql_text
+
+SELECT sysdate
+FROM dual
+/
+
+SELECT name , dbid, db_unique_name , database_role 
+, created , platform_name
+FROM v$database
+/
+
+SELECT instance_number , instance_name , instance_role
+, version , parallel , thread#
+, startup_time , host_name
+FROM v$instance
+/
+
+SELECT sql_id
+, sql_text
+FROM v$sqlarea
+WHERE sql_text LIKE '%WRK_RCP_NO%'
+/
+
+SELECT sql_id
+, sharable_mem
+, persistent_mem
+, runtime_mem
+, version_count
+, loaded_versions
+, open_versions
+, users_opening
+, executions , px_servers_executions
+, command_type
+, parsing_user_id , parsing_schema_id , parsing_schema_name
+, module , action 
+, sql_text
+FROM v$sqlarea
+WHERE sql_id = :vc_sql_id
+/
+
+set long 10000
+
+SELECT sql_id
+, sql_fulltext
+FROM v$sqlarea
+WHERE sql_id = :vc_sql_id
+/
+
+set long 80
+
+SELECT *
+FROM v$sql_shared_cursor
+WHERE sql_id = :vc_sql_id
+/
+spool off
